642bf3ce1995fb78df202dc60c523b5d36876839
henryfrstr <henryfrstr@gmail.com>
2020-04-25 00:25:21 +0300

    addition

--- armstrong.py
+++ armstrong.py
@@ -1,23 +1,28 @@
+print("Presss 'q' for quit.")
 while True:
-    number = input("Enter a positive Number:").strip()
+    number = input("Enter a positive Number:").strip().lower()
     numberofdigits = len(number)
     summ = 0
 
-    if number.isnumeric():
-
-        for i in range(numberofdigits):
-            summ += int(number[i]) ** numberofdigits
-
-        if summ == int(number):
-            print(number, "is a Amstrong Number.")
-        else:
-            print(number, "is not a Amstrong Number.")
-
-    elif number.count(".") == 1 and number.replace(".", "1").isnumeric() or number.count(",") == 1 and number.replace(",", "1").isnumeric():
-        print("Please enter a integer number.")
-
-    elif number.count("-") == 1 and number.index("-") == 0 and number.replace("-", "1").isnumeric():
-        print("Please enter a positive number")
-
+    if number == "q":
+        print("See you...")
+        break
     else:
-        print("Do not use any entries other than numeric values")
+        if number.isnumeric():
+
+            for i in range(numberofdigits):
+                summ += int(number[i]) ** numberofdigits
+
+            if summ == int(number):
+                print(number, "is a Amstrong Number.")
+            else:
+                print(number, "is not a Amstrong Number.")
+
+        elif number.count(".") == 1 and number.replace(".", "1").isnumeric() or number.count(",") == 1 and number.replace(",", "1").isnumeric():
+            print("Please enter a integer number.")
+
+        elif number.count("-") == 1 and number.index("-") == 0 and number.replace("-", "1").isnumeric():
+            print("Please enter a positive number")
+
+        else:
+            print("Do not use any entries other than numeric values")
