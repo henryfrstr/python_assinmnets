98ce94892384129cc5a634c3c94add24aba95ead
henryfrstr <henryfrstr@gmail.com>
2020-05-02 20:35:53 +0300

    is_prime

---
+++ is_prime.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+
+primes = []
+for prime in range(2, number):
+    is_prime = True
+    for num in range(2, prime):
+        if prime % num == 0:
+            is_prime = False
+    if is_prime:
+        primes.append(prime)
+print(primes)
