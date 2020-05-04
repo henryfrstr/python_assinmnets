1a52bd6758e26a520f1719cdc277c6f9755fb5cc
henryfrstr <henryfrstr@gmail.com>
2020-05-05 01:43:31 +0300

    is_prime3

--- is_prime.py
+++ is_prime.py
@@ -2,7 +2,7 @@ number = int(input("Enter a number: "))
 primes = []
 for prime in range(2, number + 1):
     is_prime = True
-    for num in range(2, prime):
+    for num in range(2, prime // 2 + 1):
         if prime % num == 0:
             is_prime = False
     if is_prime:
