64abec000115877cd6e64b20e100db5818462840
henryfrstr <henryfrstr@gmail.com>
2020-07-06 19:11:02 +0300

    gggg

--- loops.py
+++ loops.py
@@ -1,4 +1,4 @@
-import random
+
 # answer = 44
 
 # question = 'What number am I thinking of?  '
@@ -14,10 +14,22 @@ import random
 #         print('Are you a MINDREADER!!!')
 #         break
 
-a = [1, 2, 3, 4, 5]
 
-b = [2, 3, 4, 5, 6]
+# a = [1, 2, 3, 4, 5]  # (1, 6)
 
-for i, j in zip(a, b):
-    print("{} * {} = {}".format(i, j, i*j))
+# b = [6, 7, 8, 9, 10, 11, 12]
 
+# c = [11, 12, 13]
+
+# for i, j, k in zip(a, b, c):
+#     print(i, j, k)
+# a = dict(zip("abc", "cde"))
+# print(a)
+
+
+a = list(range(1, 11))
+b = list(range(1, 11))
+
+for i in a:  # 12345678910
+    for j in b:
+        print("{} * {} = {}".format(i, j, i*j))
